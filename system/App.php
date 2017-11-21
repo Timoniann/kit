df63717f60aa7f3172042c99a7c2fb0994c13eee
Timoniann <timoniann@gmail.com>
2017-11-21 13:36:41 +0200

    added 404 page handler

--- system/App.php
+++ system/App.php
@@ -21,6 +21,7 @@
 
 
 			// Redirecting type here
+			// It makes for routing admin, etc
 
 			$content = "";
 
@@ -36,7 +37,10 @@
 				$view_object = new View($controller_object->getData(), $view_path);
 				$content = $view_object->render();
 			} else{
-				echo "NOT FOUND METHOD $controller_method";
+				$view_path = VIEWS . "/404.html";
+				$view_object = new View($controller_object->getData(), $view_path);
+				$content = $view_object->render();
+				//echo "NOT FOUND METHOD $controller_method";
 				// Handle not found method
 			}
 
