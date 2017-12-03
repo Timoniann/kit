0165a0bdf0bbaecfdf0eae389a2c697f446393fc
BadTrap <iudut@bk.ru>
2017-12-03 12:15:28 +0200

    Create library Controller

---
+++ controllers/LibraryController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Viktor
+ * Date: 03.12.2017
+ * Time: 11:58
+ */
+
+
+
+
+class LibraryController extends Controller
+{
+    function index(){
+
+    }
+}
