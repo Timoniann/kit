0be8a0b235fbb09cb2f40cfc1a0d1421935e301a
BadTrap <iudut@bk.ru>
2017-12-03 21:23:02 +0200

    Fix Bag:Search by Books's Name (Title) Add Search by Subject View

--- controllers/LibraryController.php
+++ controllers/LibraryController.php
@@ -18,31 +18,32 @@ class LibraryController extends Controller
 
                 Router::redirect("/library/book_author_search/".$_POST['book_author_search']);
             } else
-            if(isset($_POST['book_author_search'])){
+                if(isset($_POST['book_name_search'])){
 
-                Router::redirect("/library/book_name_search/".$_POST['book_name_search']);
-            }
+                    Router::redirect("/library/book_name_search/".$_POST['book_name_search']);
+                }
 
 
         }
-
-    }
-    function book_author_search(){
-
-        $this->params;
-        $this->data['params'] = $this->params;
-        $books_table_author = new Books();
-        $this->data['books'] = $books_table_author->search_by_author($this->params[0]);
-
-    }
+        }
 
     function book_name_search(){
 
         $this->params;
         $this->data['params'] = $this->params;
         $books_table_name = new Books();
-        $this->data['books'] = $books_table_name->search_by_name($this->params[0]);
+        $this->data['books'] = $books_table_name->search_by_name(array_pop($this->params));
 
     }
+
+    function book_author_search(){
+
+        $this->params;
+        $this->data['params'] = $this->params;
+        $books_table_author = new Books();
+        $this->data['books'] = $books_table_author->search_by_author(array_pop($this->params));
+
+    }
+
 
 }
